cc7146e32a9cae5432141dc9c0d18063937e3ed2
chinchindaisuki <abura.chair.inu@gmail.com>
2019-02-23 22:06:40 +0900

    add triangle

---
+++ Kazuma/triangle.py
@@ -0,0 +1,6 @@
+def triangle():
+    bottom = int(input("how long is the bottom? "))
+    hight = int(input("how long is the hight? "))
+    return bottom * hight / 2
+
+print(f"triangle is {triangle()}")
