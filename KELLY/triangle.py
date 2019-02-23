9b290cc3fdc550495c8cd903ad238498ebabadda
JIM10-151005 <t.kurokawa@kobayashi-mfg.co.jp>
2019-02-23 22:25:45 +0900

    modify triamgle function

--- KELLY/triangle.py
+++ KELLY/triangle.py
@@ -1,8 +1,8 @@
 def triangle(bottom, height):
-    return float(bottom * height/2)
+    return bottom * height/2
 
 
 bottom = input("Please input bottom line length: ")
 height = input("Please input height: ")
 
-print(triangle(int(bottom), int(height)))
+print(triangle(float(bottom), float(height)))
