84eeea72feb825df8fbdd3cab5d8381a47f6326f
neetkai02 <sinsemiaoftwilight0211@yahoo.co.jp>
2019-04-06 22:57:13 +0900

    add test.py

--- taityo/test.py
+++ taityo/test.py
@@ -3,3 +3,6 @@ manko=[]
 for i in reversed(chinko):
     manko.append(i)
 print(''.join(manko))
+
+put_line = input()
+print(input_line.replace('1','January').replace('2','Feburary').replace('3','March').replace('4','April').replace('5','May').replace('6','June').replace('7','July').replace('8','August').replace('9','September').replace('10','October')in.replace('11','November').replace('12','December'))
